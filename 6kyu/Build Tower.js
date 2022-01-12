a19135c2dcc98b62f59c95846bb90e7c6e4517ff
Olivia Marolf <87673860+livmarolf@users.noreply.github.com>
2022-01-12 15:56:13 -0700

    first commit

---
+++ 6kyu/Build Tower.js
@@ -0,0 +1,44 @@
+//Build Tower:
+
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Lua: returns a Table;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ', 
+//   ' *** ', 
+//   '*****'
+// ]
+// and a tower of 6 floors looks like below
+
+// [
+//   '     *     ', 
+//   '    ***    ', 
+//   '   *****   ', 
+//   '  *******  ', 
+//   ' ********* ', 
+//   '***********'
+// ]
+
+function towerBuilder(nFloors) {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+  tower.push(" ".repeat(nFloors - i - 1)
+  + "*".repeat((i * 2)+ 1)
+  + " ".repeat(nFloors - i - 1))
+  }
+  return tower;
+}
---
+++ 7kyu/V A P O R C O D E.js
@@ -0,0 +1,11 @@
+//V A P O R C O D E:
+
+// ASC Week 1 Challenge 4 (Medium #1)
+
+// Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+// Note that spaces should be ignored in this case.
+
+function vaporcode(s) {
+  return s.toUpperCase().replace(/\s/g, "").split("").join("  ");
+}
